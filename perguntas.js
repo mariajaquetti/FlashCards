6a41a62bcf4c8e1caf607688e19b332d319e78eb
mariajaquetti <maria.jaquetti@escola.pr.gov.br>
2025-05-24 04:21:25 +0000

    Reorganização com JavaScript (módulo 6, aula 1)

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Científico',
+    'O que causa a neve colorida?',
+    'Neve colorida é um fenómeno natural em que a neve adquire uma cor diferente do branco, geralmente por causa de algas, poluição ou outros materiais que se acumulam sobre a neve.',
+)
+
+criaCartao(
+    'Científico',
+    'O que são extremófilos?',
+    'Extremófilo é um organismo capaz de viver em ambientes de condições físico-geo-químicamente extremas para a maioria dos outros seres vivos do planeta Terra.',
+)
+
+criaCartao(
+    'Científico',
+    'Por que as estrelas têm cores diferentes?',
+    'Cores diferentes das estrelas são um reflexo das suas temperaturas superficiais. Estrelas mais quentes emitem luz com cores mais azuis, enquanto as mais frias emitem luz com cores mais vermelhas.',
+)
+
+criaCartao(
+    'Científico',
+    'Por que o som não se propaga no vácuo?',
+    'O som não se propaga no vácuo porque ele é uma onda mecânica, que necessita de um meio material para se propagar. No vácuo, não há partículas para vibrar e transmitir a energia da onda sonora.',
+)
+
+criaCartao(
+    'Científico',
+    'Como o sol causa mutações genéticas?',
+    'A luz solar, em particular a radiação ultravioleta (UV), pode causar mutações genéticas ao danificar a estrutura do DNA. Essa radiação pode formar lesões no DNA, como dímeros de pirimidina, que podem levar a alterações na sequência genética.',
+)
+
+criaCartao(
+    'Científico',
+    'Por que o sangue de alguns animais é azul?',
+    'O sangue azul em alguns animais, como aranhas, caranguejos e moluscos, é resultado da presença de uma proteína chamada hemocianina, que transporta oxigênio no lugar da hemoglobina que transporta oxigênio no sangue humano.',
+)
+
+criaCartao(
+    'Científico',
+    'O que é a hibernação dos animais?',
+    'A hibernação é um estado de profunda dormência que alguns animais entram para sobreviver em condições ambientais adversas, principalmente o frio e a escassez de alimento.',
+)
+
+criaCartao(
+    'Científico',
+    'O que são Estômatos?',
+    'Os estômatos são aberturas microscópicas na epiderme das folhas, e também em alguns caules e frutos jovens, que permitem a troca de gases (oxigénio, dióxido de carbono e vapor de água) entre a planta e o ambiente.',
+)
+
+criaCartao(
+    'Científico',
+    'O que é nanokicking?',
+    'Nanokicking é uma técnica científica que utiliza altas frequências para estimular células-tronco a se transformarem em células produtoras de ossos.',
+)
